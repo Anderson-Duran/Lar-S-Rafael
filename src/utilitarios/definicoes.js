63db5494101c736e540d823eeee258fc487a3075
Rodrigo Fernandes Monteiro <98911448+RodrigoFMmonteiro@users.noreply.github.com>
2023-12-02 17:21:27 -0300

    inserção sugestão, projeto e prestador.

--- src/utilitarios/definicoes.js
+++ src/utilitarios/definicoes.js
@@ -1,3 +1,5 @@
 export const urlBase = "https://129.146.68.51/aluno38-pfsii/professor";
 export const urlBase2 = "https://129.146.68.51/aluno38-pfsii/turma";
 export const urlBase3 = "https://129.146.68.51/aluno38-pfsii/curso";
+
+export const urlBaseSugest = "http://localhost:4039";
