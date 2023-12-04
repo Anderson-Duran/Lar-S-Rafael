55072c1d8297b7abf17d48b20342ab3035e9d5ed
RenatoAF <renato012br@hotmail.com>
2023-12-04 12:36:06 -0300

    Backend Local Renato

--- src/utilitarios/definicoes.js
+++ src/utilitarios/definicoes.js
@@ -1,5 +1,8 @@
-export const urlBase = "https://129.146.68.51/aluno38-pfsii/professor";
-export const urlBase2 = "https://129.146.68.51/aluno38-pfsii/turma";
-export const urlBase3 = "https://129.146.68.51/aluno38-pfsii/curso";
+//export const urlBase = "https://129.146.68.51/aluno38-pfsii/professor";
+//export const urlBase2 = "https://129.146.68.51/aluno38-pfsii/turma";
+//export const urlBase3 = "https://129.146.68.51/aluno38-pfsii/curso";
+export const urlBase = "http://localhost:3001/professor";
+export const urlBase2 = "http://localhost:3001/turma";
+export const urlBase3 = "http://localhost:3001/curso";
 
 export const urlBaseSugest = "http://localhost:4039";
