91af1cfb7492b906009f544249a06f3307f35450
Anderson-Duran <andersonduran@hotmail.com>
2024-02-14 17:01:12 -0300

    changing url

--- src/assets/definicoes.js
+++ src/assets/definicoes.js
@@ -1,7 +1,7 @@
-export const urlBase = "https://129.146.68.51/aluno5-pfsii/cadastroPaciente/pacients";
-export const urlUser = "https://129.146.68.51/aluno5-pfsii/users";
-/* export const urlBase = "https://back-fsii.vercel.app/cadastroPaciente/pacients"; */
-/* export const urlUser = "https://back-fsii.vercel.app/users"; */
+/*export const urlBase = "https://129.146.68.51/aluno5-pfsii/cadastroPaciente/pacients";
+export const urlUser = "https://129.146.68.51/aluno5-pfsii/users";*/
+export const urlBase = "https://back-fsii.vercel.app/cadastroPaciente/pacients"; 
+export const urlUser = "https://back-fsii.vercel.app/users"; 
 /* export const urlBase = "http://localhost:4005/cadastroPaciente/pacients"
 export const urlUser = "https://localhost:4005/users"; */
 
